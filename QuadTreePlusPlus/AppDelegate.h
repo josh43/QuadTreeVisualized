1415ba216d61da8b73af7eb749e6488e5091208b
joshua <josh@joshuas-imac.local>
2016-06-08 21:06:02 -0700

    Initial Commit

---
+++ QuadTreePlusPlus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuadTreePlusPlus
+//
+//  Created by joshua on 6/8/16.
+//  Copyright © 2016 joshua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuadTreePlusPlus/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuadTreePlusPlus
+//
+//  Created by joshua on 6/8/16.
+//  Copyright © 2016 joshua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
